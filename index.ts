02ae9a94dcd9b4e60ee426bcfc5a620d6fbf6117
Russell Blickhan <rwblickhan@gmail.com>
2023-10-07 16:57:03 -0700

    Set up index to tie everything together

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
 import { Hono } from "hono";
-import { setupRoutes } from "./server";
+import { setupRoutes } from "./routes";
+import { TodoContext, TodoMetadata, generateMetadata } from "./chat";
+
+const todoContexts: TodoContext[] = [
+  {
+    filename: "app.tsx",
+    code: `// TODO: app.asana.com/id/78023489
+// Implement the final prompt
+const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: prompt }],
+    model: "gpt-3.5-turbo",
+  });`,
+  },
+];
+const todoMetadata: TodoMetadata[] = [];
+for (const context of todoContexts) {
+  const metadata = await generateMetadata(context);
+  todoMetadata.push(metadata);
+}
 
 const app = new Hono();
-setupRoutes(app, []);
+setupRoutes(app, todoMetadata);
+console.log("Ready!");
 
 export default app;
